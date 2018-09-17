d1f28c56a4aee8dc5599a782a4286c3f9e850655
Ernie Barber <erniebarber@me.com>
2018-09-17 13:46:43 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -15,13 +15,19 @@ numberofnodes = 4
 #create a LAN
 link = request.LAN("lan")
 
-#Loop and create nodes
-for i in range(1, numberofnodes):
+#Loop and create nodes.  
+for i in range(1, numberofnodes + 1):
+    # create and label each node
     node = request.XenVM("node-" + str(i))
+    # Install the OS
     node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+    # Assign the node to the interface
     intrface = node.addInterface("if" + str(i))
+    #assign the 
     intrface.component_id = "eth1" 
-    intrface.addAddress(rspec.IPv4Address("192.168.1." + str(i), "255.255.255.0"))
+    #Assign an IP address to the interface
+    intrface.addAddress(pg.IPv4Address("192.168.1." + str(i), "255.255.255.0"))
+    #add the interface to the LAN
     link.addInterface(intrface)
     #set Node-1 to a public IP
     if i == 1:
